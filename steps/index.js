f510c984212045cd9eeaad79935badd6676288b6
i3oc9i <ivano.coltellacci@gmail.com>
2024-09-03 16:13:45 +0200

    chore: Refactor nbOfSteps naming

--- steps/index.js
+++ steps/index.js
@@ -1,7 +1,7 @@
 const nextBtn = document.getElementById("next");
 const prevBtn = document.getElementById("prev");
 
-const nbOfSteps = document.querySelectorAll(".step");
+const stepsElements = document.querySelectorAll(".step");
 
 const progressBar = document.querySelector(".progress-bar-front");
 
@@ -9,8 +9,8 @@ let currentStep = 1;
 
 nextBtn.addEventListener("click", () => {
   currentStep += 1;
-  if (currentStep > nbOfSteps.length) {
-    currentStep = nbOfSteps.length;
+  if (currentStep > stepsElements.length) {
+    currentStep = stepsElements.length;
   }
   updateStepProgress();
 });
@@ -25,18 +25,18 @@ prevBtn.addEventListener("click", () => {
 
 function updateStepProgress() {
   prevBtn.disabled = currentStep === 1 ? true : false;
-  nextBtn.disabled = currentStep === nbOfSteps.length;
+  nextBtn.disabled = currentStep === stepsElements.length;
 
-  nbOfSteps.forEach((step, index) => {
+  stepsElements.forEach((step, index) => {
     if (index < currentStep) {
       step.classList.add("checked");
       step.innerHTML = `<i class="fas fa-check"></i>
-      <small>${index === 0 ? "Start" : index === nbOfSteps.length - 1 ? "Final" : "Step " + index}</small>`;
+      <small>${index === 0 ? "Start" : index === stepsElements.length - 1 ? "Final" : "Step " + index}</small>`;
     } else {
       step.classList.remove("checked");
       step.innerHTML = `<i class="fas fa-times"></i>`;
     }
   });
 
-  progressBar.style.width = `${((currentStep - 1) / (nbOfSteps.length - 1)) * 100}%`;
+  progressBar.style.width = `${((currentStep - 1) / (stepsElements.length - 1)) * 100}%`;
 }
